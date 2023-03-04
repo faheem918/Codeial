c71a360011deacd4774d4a44961ce16c2b8057b3
Faheem Ahmad <faheem.ahmad3443@gmail.com>
2023-03-04 18:48:06 +0530

    setup passport js for authentication

--- index.js
+++ index.js
@@ -1,33 +1,55 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-
 const app = express();
 const port = 8000;
-const expressLayout = require('express-ejs-layouts');
+const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
-
-app.use(express.static('./assets'));
+// used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
 
 app.use(express.urlencoded());
 
 app.use(cookieParser());
 
-app.use(expressLayout);
+app.use(express.static('./assets'));
 
-//extract style and scripts from sub pages into the layout
+app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
-// use express router 
-app.use('/', require('./routes'));
 
-// setup up the view engine
+
+
+// set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+
+app.use(session({
+    name: 'codeial',
+    // TODO change the secret before deployment in production mode
+    secret: 'blahsomething',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// use express router
+app.use('/', require('./routes'));
+
+
 app.listen(port, function(err){
-   if(err){
-      console.log(`error in the server :${err}`);
-   }
-   console.log(`server is running on port : ${port}`);
+    if (err){
+        console.log(`Error in running the server: ${err}`);
+    }
+
+    console.log(`Server is running on port: ${port}`);
 });
