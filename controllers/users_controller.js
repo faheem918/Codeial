ca93647aa9394269fae4ed9b56218247235f8197
Faheem Ahmad <faheem.ahmad3443@gmail.com>
2023-03-08 18:01:20 +0530

    Create Sign Out and A Usable Header

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,12 +3,17 @@ const User = require('../models/user');
 module.exports.profile = function(req, res){
     // res.end('<h1>User Profile</h1>');
     return res.render('user_profile',{
-        title:"User Profile"
+        title:"User Profile",
+        user:req.user
      });
 }
 
 // render the sign up page
 module.exports.signUp = function(req, res){
+
+    if (req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
     return res.render('user_sign_up', {
         title: "Codeial | Sign Up"
     })
@@ -17,6 +22,10 @@ module.exports.signUp = function(req, res){
 
 // render the sign in page
 module.exports.signIn = function(req, res){
+    if (req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
     return res.render('user_sign_in', {
         title: "Codeial | Sign In"
     })
@@ -48,5 +57,11 @@ module.exports.create = function(req, res){
 // sign in and create a session for the user
 module.exports.createSession = function(req, res){
     // TODO later
+    return res.redirect('/');
+}
+
+module.exports.destroySession = function(req, res){
+    req.logout();
+
     return res.redirect('/');
 }
